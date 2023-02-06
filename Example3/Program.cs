4d9732c541d75ff66151bff84ebf95e8bdb4f58d
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-06 12:52:27 +0300

    нахождение индекса элемента массива, удовлетворяющего заданному условию

---
+++ Example3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = {1, 12, 31,4, 18, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n )
+{
+
+if (array[index] == find)
+{
+    System.Console.WriteLine(index);
+    // break- для остановки, после нахождения первого подходящего
+    // значения
+    break;
+}
+   // index = index + 1;  а можно и так:
+   index++;
+}
